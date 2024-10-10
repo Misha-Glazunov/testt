72d152c770abb67fc9091d368dd14dec69cc9873
Глазунов Михаил <mishgun.gl@gmail.com>
2024-10-10 20:49:28 +0300

    First commit

---
+++ src/test.java
@@ -0,0 +1,21 @@
+public class test {
+    static String Example() {
+        int y;
+        boolean x;
+        if (x = false) {
+            String t = "Выберите другое имя пользователся";
+            return t;
+        }
+        else
+        {
+            String f = "Отличное имя";
+            return f;
+        }
+    }
+    public static void main(String[] args) {
+        String s2 = "иван иванов";
+        String s1 = "Игорь";
+        boolean x = s2.equals(s1);
+        System.out.println(test.Example());
+    }
+}
